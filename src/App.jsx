a225c0ec96b08ebde38c8db792f48ba225f8561e
Dheeraj Kumar Gaur <93270093+dheerajkumargaur@users.noreply.github.com>
2024-07-08 14:46:52 +0530

    rain pattern

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import RainDrop from "./components/rainDrop/rainDrop.jsx";
+const App = () => {
+  return (
+    <div>
+      <RainDrop />
+    </div>
+  );
+};
+
+export default App;
